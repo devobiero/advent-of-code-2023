8a0c0a7ed185c849c341a906f58fe65b138f9825
george <georgeobiero2@gmail.com>
2023-12-23 15:23:04 +0300

    chore: Day 8 - part 2

--- day_08/main.py
+++ day_08/main.py
@@ -1,3 +1,6 @@
+import math
+
+
 def part_one(filename):
     steps, nodes = load_maps(filename)
     lookup = dict()
@@ -20,7 +23,44 @@ def part_one(filename):
 
 
 def part_two(filename):
-    pass
+    steps, nodes = load_maps(filename)
+    lookup = dict()
+
+    for node in nodes:
+        dirs = node.split(" = ")
+        lookup.update({dirs[0]: tuple(dirs[1][1:-1].split(", "))})
+
+    positions = [node for node in lookup if node.endswith("A")]
+
+    cycles = []
+    for current in positions:
+        cycle = []
+        current_steps = steps
+        step_count = 0
+        first_z = None
+
+        while True:
+            while step_count == 0 or not current.endswith("Z"):
+                step_count += 1
+                current = (
+                    lookup.get(current, None)[0]
+                    if current_steps[0] == "L"
+                    else lookup.get(current, None)[1]
+                )
+                current_steps = current_steps[1:] + current_steps[0]
+
+            cycle.append(step_count)
+
+            if first_z is None:
+                first_z = current
+                step_count = 0
+            elif current == first_z:
+                break
+
+        cycles.append(cycle)
+
+    nums = [cycle[0] for cycle in cycles]
+    return math.lcm(*nums)
 
 
 def load_maps(filename):
