e99c2bcd138153713e4763e0a03922e68e2e18d4
george <georgeobiero2@gmail.com>
2023-12-30 13:01:45 +0300

    chore: Day 10 - part 2

--- day_10/main.py
+++ day_10/main.py
@@ -67,7 +67,121 @@ def part_one(filename):
 
 
 def part_two(filename):
-    pass
+    grid = load_grid(filename)
+
+    for r, row in enumerate(grid):
+        for c, ch in enumerate(row):
+            if ch == "S":
+                sr = r
+                sc = c
+                break
+
+    # use bfs for path finding
+    loop = set({(sr, sc)})
+    q = deque([(sr, sc)])
+
+    maybe_s = set({"|", "-", "7", "J", "L", "F"})
+
+    while q:
+        r, c = q.popleft()
+        ch = grid[r][c]
+
+        if (
+            r > 0
+            and ch in "S|JL"
+            and grid[r - 1][c] in "|7F"
+            and (r - 1, c) not in loop
+        ):
+            loop.add((r - 1, c))
+            q.append((r - 1, c))
+
+            # set intersection
+            if ch == "S":
+                maybe_s &= {"|", "J", "L"}
+
+        if (
+            r < len(grid) - 1
+            and ch in "S|7F"
+            and grid[r + 1][c] in "|JL"
+            and (r + 1, c) not in loop
+        ):
+            loop.add((r + 1, c))
+            q.append((r + 1, c))
+
+            if ch == "S":
+                maybe_s &= {"|", "7", "F"}
+
+        if (
+            c > 0
+            and ch in "S-J7"
+            and grid[r][c - 1] in "-LF"
+            and (r, c - 1) not in loop
+        ):
+            loop.add((r, c - 1))
+            q.append((r, c - 1))
+
+            if ch == "S":
+                maybe_s &= {"-", "J", "7"}
+
+        if (
+            c < len(grid[0]) - 1
+            and ch in "S-LF"
+            and grid[r][c + 1] in "-J7"
+            and (r, c + 1) not in loop
+        ):
+            loop.add((r, c + 1))
+            q.append((r, c + 1))
+
+            if ch == "S":
+                maybe_s &= {"-", "L", "F"}
+
+    assert len(maybe_s) == 1
+    (S,) = maybe_s
+
+    # replace "S" with correct char
+    grid = [row.replace("S", S) for row in grid]
+
+    # replace garbage pipes with dots
+    grid = [
+        "".join(ch if (r, c) in loop else "." for c, ch in enumerate(row))
+        for r, row in enumerate(grid)
+    ]
+
+    # invalid / outside points
+    outside = set()
+
+    for r, row in enumerate(grid):
+        up = None
+        within = False
+        for c, ch in enumerate(row):
+            # Check vertical pipe
+            if ch == "|":
+                assert up is None
+                within = not within
+            # Horizontal pipe
+            elif ch == "-":
+                assert up is not None
+            elif ch in "LF":
+                assert up is None
+                up = ch == "L"
+            elif ch in "J7":
+                assert up is not None
+                if ch != ("J" if up else "7"):
+                    within = not within
+                up = None
+            elif ch == ".":
+                pass
+            else:
+                raise RuntimeError(f"Unexpected horizontal character: {ch}")
+            if not within:
+                outside.add((r, c))
+
+    for r, row in enumerate(grid):
+        for c, ch in enumerate(row):
+            print("#" if (r, c) in outside - loop else ".", end="")
+        print()
+
+    return len(grid) * len(grid[0]) - len(outside | loop)
 
 
 def load_grid(filename):
